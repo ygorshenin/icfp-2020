67b8dc8ff1868a7c6abd1fc30736f7f522794547
Maxim Pimenov <m@maps.me>
2020-07-18 02:53:04 +0300

    Check useless variables, replace constants and integer negations.

--- deobfuscator.go
+++ deobfuscator.go
@@ -4,7 +4,9 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -52,8 +54,86 @@ func writeProgram(prog []Decl, out io.Writer) {
 	}
 }
 
+func checkUseless(prog []Decl) {
+	leftSides := make(map[string]bool)
+	used := make(map[string]bool)
+	for _, decl := range prog {
+		if _, ok := leftSides[decl.lhs]; ok {
+			panic("double decl")
+		}
+		leftSides[decl.lhs] = true
+		for _, tok := range decl.rhs {
+			used[tok] = true
+		}
+	}
+
+	used["galaxy"] = true
+	for lhs, _ := range leftSides {
+		if _, ok := used[lhs]; !ok {
+			panic("unused decl:" + lhs)
+		}
+	}
+}
+
+func isInt(s string) bool {
+	_, err := strconv.Atoi(s)
+	return err == nil
+}
+
+func replaceConstants(prog []Decl) (resProg []Decl) {
+	vals := make(map[string]string)
+
+	for _, decl := range prog {
+		if len(decl.rhs) != 1 {
+			resProg = append(resProg, decl)
+			continue
+		}
+		x := decl.rhs[0]
+		if isInt(x) {
+			log.Println("found constant: " + decl.lhs + " = " + x)
+			vals[decl.lhs] = x
+		} else {
+			resProg = append(resProg, decl)
+		}
+	}
+
+	for i, decl := range resProg {
+		for j, tok := range decl.rhs {
+			if val, ok := vals[tok]; ok {
+				resProg[i].rhs[j] = val
+			}
+		}
+	}
+
+	return
+}
+
+func replaceIntegerNegations(prog []Decl) (resProg []Decl) {
+	for _, decl := range prog {
+		var newRhs []string
+		for j := 0; j < len(decl.rhs); {
+			if j+2 < len(decl.rhs) && decl.rhs[j] == "ap" && decl.rhs[j+1] == "neg" && isInt(decl.rhs[j+2]) {
+				log.Println("negating " + decl.rhs[j+2])
+				newRhs = append(newRhs, "-"+decl.rhs[j+2])
+				j += 3
+			} else {
+				newRhs = append(newRhs, decl.rhs[j])
+				j += 1
+			}
+		}
+		resProg = append(resProg, Decl{lhs: decl.lhs, rhs: newRhs})
+	}
+	return
+}
+
 func main() {
+	log.SetFlags(0)
+
 	prog := readProgram("./galaxy.txt")
+
+	checkUseless(prog)
+	prog = replaceConstants(prog)
+	prog = replaceIntegerNegations(prog)
 
 	writeProgram(prog, os.Stdout)
 }
